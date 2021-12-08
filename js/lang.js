d4a3c521cd272b86d0364c9ea5836d49b3cb3eb1
sabrinaveit <sabriveit@gmail.com>
2021-12-08 10:40:28 -0500

    Initial commit

---
+++ js/lang.js
@@ -0,0 +1,95 @@
+var button = document.querySelector('button1','button2','button3','button4','button5','button6','button7','button8','button9','button10');
+var audio = document.querySelector('audio1', 'audio2', 'audio3', 'audio4', 'audio5', 'audio6', 'audio7', 'audio8', 'audio9', 'audio10');
+
+audio1.style.display = 'none';
+audio2.style.display = 'none';
+audio3.style.display = 'none';
+audio4.style.display = 'none';
+audio5.style.display = 'none';
+audio6.style.display = 'none';
+audio7.style.display = 'none';
+audio8.style.display = 'none';
+audio9.style.display = 'none';
+audio10.style.display = 'none';
+
+button1.onclick = () => {
+  if (audio1.style.display === 'none') {
+    audio1.style.display = 'block';
+  } else {
+    audio1.style.display = 'none';
+  }
+};
+
+
+button2.onclick = () => {
+  if (audio2.style.display === 'none') {
+    audio2.style.display = 'block';
+  } else {
+    audio2.style.display = 'none';
+  }
+};
+
+button3.onclick = () => {
+  if (audio3.style.display === 'none') {
+    audio3.style.display = 'block';
+  } else {
+    audio3.style.display = 'none';
+  }
+};
+
+button4.onclick = () => {
+  if (audio4.style.display === 'none') {
+    audio4.style.display = 'block';
+  } else {
+    audio4.style.display = 'none';
+  }
+};
+
+button5.onclick = () => {
+  if (audio5.style.display === 'none') {
+    audio5.style.display = 'block';
+  } else {
+    audio5.style.display = 'none';
+  }
+};
+
+button6.onclick = () => {
+  if (audio6.style.display === 'none') {
+    audio6.style.display = 'block';
+  } else {
+    audio6.style.display = 'none';
+  }
+};
+
+
+button7.onclick = () => {
+  if (audio7.style.display === 'none') {
+    audio7.style.display = 'block';
+  } else {
+    audio7.style.display = 'none';
+  }
+};
+
+button8.onclick = () => {
+  if (audio8.style.display === 'none') {
+    audio8.style.display = 'block';
+  } else {
+    audio8.style.display = 'none';
+  }
+};
+
+button9.onclick = () => {
+  if (audio9.style.display === 'none') {
+    audio9.style.display = 'block';
+  } else {
+    audio9.style.display = 'none';
+  }
+};
+
+button10.onclick = () => {
+  if (audio10.style.display === 'none') {
+    audio10.style.display = 'block';
+  } else {
+    audio10.style.display = 'none';
+  }
+};
---
+++ js/places.js
@@ -0,0 +1,55 @@
+places1 =
+[{"placeName":"North America ","descrip":"United States, Canada, Jamaica, Barbados, Bahamas","latitude":37.0902,"longitude":-95.7129},
+{"placeName":"Europe","descrip":"United Kingdom, Republic of Ireland, Malta","latitude":53.0000,"longitude":9.0000},
+{"placeName":"South America","descrip":"Trinidad and Tobago, Guyana","latitude":8.7832,"longitude":-55.4915},
+{"placeName":"Oceania","descrip":"Australia, New Zealand, Papua New Guinea, Fiji, Samoa, Tonga, Solomon Islands, Micronesia, Vanuatu, Kiribati","latitude":30.0000,"longitude":140.0000},
+{"placeName":"Asia","descrip":"India, Pakistan, Singapore, Philippines, Sri Lanka, Malaysia","latitude":34.0479,"longitude":100.6197},
+{"placeName":"Africa","descrip":"South Africa, Nigeria, Cameroon, Kenya, Zimbabwe, Ghana, Rwanda, Sudan, Botswana, Ethiopia","latitude":8.7832,"longitude":34.5085}]
+
+
+places2 =
+[{"placeName":"Oceania","descrip":"Australia","latitude":-25.2744,"longitude":133.7751},
+{"placeName":"Asia","descrip":"Christmas Island, China, Hong Kong, Macau, Malaysia, Singapore, Taiwan","latitude":34.0479,"longitude":100.6197}]
+
+places3 =
+[{"placeName":"South America ","descrip":"Guyana,Suriname,Trinidad and Tobago","latitude":8.7832,"longitude":-55.4915},
+{"placeName":"Oceania","descrip":"Australia, Fiji, New Zealand","latitude":30.0000,"longitude":140.0000},
+{"placeName":"Asia","descrip":"India, United Arab Emirates","latitude":34.0479,"longitude":100.6197}]
+
+places4 =
+[{"placeName":"North America ","descrip":"Belize, Canada, Cayman Islands, Costa Rica, Cuba, Dominican Republic, El Salvador, Guatemala, Honduras, Mexico, Nicaragua, Panama, Puerto Rico, Saint Martin, Sint Maarten, United States, Virgin Islands","latitude":37.0902,"longitude":-95.7129},
+{"placeName":"Europe","descrip":"Gibraltar, Spain, Switzerland","latitude":53.0000,"longitude":9.0000},
+{"placeName":"South America ","descrip":"Argentina, Aruba, Bolivia, Brazil, Chile, Colombia, Trinidad and Tobago, Curacao, Ecuador, Paraguay, Peru, Uruguay, Venezuela","latitude":8.7832,"longitude":-55.4915},
+{"placeName":"Africa","descrip":"South Africa, Nigeria, Cameroon, Kenya, Zimbabwe, Ghana, Rwanda, Sudan, Botswana, Ethiopia","latitude":8.7832,"longitude":34.5085}]
+
+places5 =
+[{"placeName":"North America ","descrip":"Canada, Panama","latitude":37.0902,"longitude":-95.7129},
+{"placeName":"Europe","descrip":"Cyprus","latitude":53.0000,"longitude":9.0000},
+{"placeName":"Oceania","descrip":"Australia","latitude":-25.2744,"longitude":133.7751},
+{"placeName":"Asia","descrip":"Afghanistan, Bahrain, Gaza Strip, Iran, Iraq, Israel, Jordan,  Kuwait, Lebanon, Maldives, Oman, Saudi Arabia, Syria, United Arab Emirates, West Bank","latitude":34.0479,"longitude":100.6197},
+{"placeName":"Africa","descrip":"Algeria, Chad, Comoros, Djibouti, Egypt, Eritrea, Ethiopia, Libya, Mauritania, Morocco, Qatar, Somalia, South Sudan, Sudan, Tanzania, Tunisia, Uganda","latitude":8.7832,"longitude":34.5085}]
+
+places6 =
+[{"placeName":"Asia","descrip":"Bangladesh, India","latitude":34.0479,"longitude":100.6197}]
+
+places7 =
+[{"placeName":"North America ","descrip":"Canada, Dominica, Grenada, Haiti, Virgin Islands, Sint Maarten, Saint Vincent and the Grenadines, Saint Pierre and Miquelon, Saint Martin, Saint Lucia, Saint Barthélemy","latitude":37.0902,"longitude":-95.7129},
+{"placeName":"Asia","descrip":"Egypt, Laos, Lebanon, Vietnam, Syria","latitude":34.0479,"longitude":100.6197},
+{"placeName":"Europe","descrip":"Andorra, France, Guernsey, Vatican City, Hungary, Switzerland, Monaco, Luxembourg","latitude":53.0000,"longitude":9.0000},
+{"placeName":"South America ","descrip":"Aruba, Curacao, Trinidad and Tobago","latitude":8.7832,"longitude":-55.4915},
+{"placeName":"Africa","descrip":"Algeria, Benin, Burkina Faso, Burundi, Cameroon, Chad, Comoros, Congo, Côte d'Ivoire, Djibouti, Egypt, Gabon, Guinea, Tunisia, Togo, Seychelles, Senegal, Sao Tome and Principe, Rwanda, Niger, Morrocco, Mauritius, Mauritania, Mali, Madagascar","latitude":8.7832,"longitude":34.5085},
+{"placeName":"Oceania","descrip":"French Polynesia, Wallis and Futuna, Vanuatu, New Caledonia","latitude":-25.2744,"longitude":133.7751}]
+
+places8 =
+[{"placeName":"Europe","descrip":"Azerbaijan, Belarus, Cyprus, Estonia, Finland, Georgia, Hungary, Kazakhstan, Latvia, Lithuania, Moldova, Poland, Russia, Svalbard, Ukraine","latitude":53.0000,"longitude":9.0000},
+{"placeName":"Asia","descrip":"Armenia, Azerbaijan, Georgia, Kazakhstan, Kyrgyzstan, Mongolia, Russia, Tajikistan, Turkmenistan, Uzbekistan","latitude":34.0479,"longitude":100.6197}]
+
+places9 =
+[{"placeName":"North America ","descrip":"Bermuda","latitude":37.0902,"longitude":-95.7129},
+{"placeName":"Asia","descrip":"Macau, Timor-Leste","latitude":34.0479,"longitude":100.6197},
+{"placeName":"Europe","descrip":"Andorra, Jersey, Liechtenstein, Luxembourg, Portugal, Switzerland","latitude":53.0000,"longitude":9.0000},
+{"placeName":"South America ","descrip":"Aruba, Brazil, Curacao, Paraguay, Sint Maarten","latitude":8.7832,"longitude":-55.4915},
+{"placeName":"Africa","descrip":"Angola, Cabo Verde, Equatorial Guinea, Gibraltar, Guinea-Bissau, Mozambique, Sao Tome and Principe","latitude":8.7832,"longitude":34.5085}]
+
+places10 =
+[{"placeName":"Asia","descrip":"Afghanistan, Bahrain, India, Nepal, Oman, Pakistan, United Arab Emirates","latitude":34.0479,"longitude":100.6197}]
